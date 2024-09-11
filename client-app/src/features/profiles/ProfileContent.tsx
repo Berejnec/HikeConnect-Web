c188ec50a66165457111e0e8f740a19fc6d29d68
Adrian Berejnec <adrianberejnec29@gmail.com>
2024-09-11 20:55:13 +0300

    Adding following feature on client side

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -3,12 +3,15 @@ import { Tab, TabPane } from "semantic-ui-react";
 import ProfilePhotos from "./ProfilePhotos";
 import { Profile } from "../../app/models/profile";
 import { observer } from "mobx-react-lite";
+import ProfileFollowings from "./ProfileFollowings";
+import { useStore } from "../../app/stores/store";
 
 interface Props {
   profile: Profile;
 }
 
 export default observer(function ProfileContent({ profile }: Props) {
+  const { profileStore } = useStore();
   const panes = [
     {
       menuItem: "About",
@@ -24,12 +27,20 @@ export default observer(function ProfileContent({ profile }: Props) {
     },
     {
       menuItem: "Followers",
-      render: () => <TabPane>Followers Content</TabPane>,
+      render: () => <ProfileFollowings />,
     },
     {
       menuItem: "Following",
-      render: () => <TabPane>Following Content</TabPane>,
+      render: () => <ProfileFollowings />,
     },
   ];
-  return <Tab menu={{ fluid: true, vertical: true }} menuPosition="right" panes={panes} style={{ paddingTop: 30 }} />;
+  return (
+    <Tab
+      menu={{ fluid: true, vertical: true }}
+      menuPosition="right"
+      panes={panes}
+      style={{ paddingTop: 30 }}
+      onTabChange={(e, data) => profileStore.setActiveTab(data.activeIndex)}
+    />
+  );
 });
